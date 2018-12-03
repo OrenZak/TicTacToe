ac66c17643b82b963e0645fc038ed32558771cec
OrenZak <orenzak24@gmail.com>
2018-12-03 22:12:30 +0200

    update the model

--- src/model/IBoard.java
+++ src/model/IBoard.java
@@ -3,12 +3,16 @@ package model;
 public interface IBoard {
 	
 	void initBoard();
-	boolean winningAchievement(IPlayer currentPlayer);
-	boolean isLegalMove (IRubric rubric, IPlayer currentPlayer) throws NotYourTurnException, FullRubricException;
-	ChosenRubricData chooseRubric (IRubric rubric);
-	String updateCurrentPlayer (IPlayer currentPlayer);
-	boolean checkDraw();
-	@Override
-	boolean equals(Object obj);
 	
+	boolean winningAchievement(IPlayer currentPlayer);
+	
+	boolean isLegalMove (IRubric rubric, IPlayer currentPlayer) throws NotYourTurnException, FullRubricException;
+	
+	ChosenRubricData chooseRubric (IRubric rubric);
+	
+	String updateCurrentPlayer (IPlayer currentPlayer);
+	
+	boolean checkDraw();
+
+	VictoryData getVictoryDetails();
 }
